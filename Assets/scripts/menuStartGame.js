0ac95f2448218ad0ddf3a20c55e2908ee16bfe24
adrian perez marrero <apematica@gmail.com>
2015-04-30 23:26:26 +0100

    Menus

--- Assets/scripts/menuStartGame.js
+++ Assets/scripts/menuStartGame.js
@@ -1,5 +1,37 @@
 ﻿#pragma strict
+public var Main_Panel:GameObject;
+public var Credits_Panel:GameObject;
+public var Difficulty_Panel:GameObject;
+var easy=false;
+var normal=false;
+var difficulty:enemyscript;
 
-function StartGame () {
+function EStartGame () {
 Application.LoadLevel("mainscene");
+difficulty.speed=-5;
+}
+
+function NStartGame () {
+Application.LoadLevel("mainscene");
+difficulty.speed=-10;
+}
+
+function HStartGame () {
+Application.LoadLevel("mainscene");
+difficulty.speed=-15;
+}
+
+function Credit (){
+Main_Panel.SetActive(false);
+Difficulty_Panel.SetActive(false);
+}
+function BackCredit(){
+Credits_Panel.SetActive(false);
+Main_Panel.SetActive(true);
+}
+function ChooseDifficulty()
+{
+Main_Panel.SetActive(false);
+Credits_Panel.SetActive(false);
+Difficulty_Panel.SetActive(true);
 }
